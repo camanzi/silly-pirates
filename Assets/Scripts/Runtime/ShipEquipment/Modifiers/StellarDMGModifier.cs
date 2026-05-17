62066b8f15b0b4588fad1a0387f74e5173f361a3
Lorenzo Camanzi <lorenzo.camanzi@gmail.com>
2026-05-17 23:01:39 +0200

    Riorganizzazione folders

---
+++ Assets/Scripts/Runtime/ShipEquipment/Modifiers/StellarDMGModifier.cs
@@ -0,0 +1,25 @@
+public class StellarDMGModifier : IDMGTypeModifier
+{
+    private readonly ShootingEquipment _equipment;
+    private readonly TurnAgentEventChannel _channel;
+    private readonly ITurnAgent _owner;
+    private bool _active = true;
+
+    public StellarDMGModifier(ShootingEquipment equipment, TurnAgentEventChannel channel, ITurnAgent owner)
+    {
+        _equipment = equipment;
+        _channel = channel;
+        _owner = owner;
+        _channel.OnEventRaised += OnTurnEnded;
+    }
+
+    public DamageType GetDMGTypeOverride() => DamageType.Stellar;
+
+    private void OnTurnEnded(ITurnAgent agent)
+    {
+        if (agent != _owner || !_active) return;
+        _active = false;
+        _equipment.RemoveDMGTypeModifier(this);
+        _channel.OnEventRaised -= OnTurnEnded;
+    }
+}
